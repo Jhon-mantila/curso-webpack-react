28164a10c7e4224a13a0dc18206f4554f02e2b17
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2023-03-29 20:38:49 -0500

    Iniciando webpack + react

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { ReactDOM } from "react-dom";
+import App from './components/App.jsx';
+//mostrar dentro un archivo html la aplicación
+ReactDOM.render(<App />, document.getElementById('app'));

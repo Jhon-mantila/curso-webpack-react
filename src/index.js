9e027e320fd56bc4508387faabcffb77dcd41623
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2023-03-30 15:43:26 -0500

    Condiguración webpack html y server local

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactDOM } from "react-dom";
+import ReactDOM  from "react-dom";
 import App from './components/App.jsx';
 //mostrar dentro un archivo html la aplicación
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('app'));
